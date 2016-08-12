b55dc7166609ee4b7e6b64c2aa4ccf12c44cabb6
Enrique Garduno <enriquegarduno@Macintosh.local>
2016-08-12 18:05:59 -0500

    sql files

---
+++ models/users.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var users = sequelize.define('users', {
+    name: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    city: DatTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return users;
+};
